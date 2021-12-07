b47335576fe72303133f892bfe855f7735d68812
Conqcd <411277487@qq.com>
2021-12-07 10:48:45 +0800

    third party

---
+++ Rengine/log.hpp
@@ -0,0 +1,23 @@
+#include "core.hpp"
+
+namespace Rengin
+{
+class log
+{
+private:
+    /* data */
+public:
+    log(/* args */);
+    ~log();
+};
+
+log::log(/* args */)
+{
+}
+
+log::~log()
+{
+}
+
+
+}
