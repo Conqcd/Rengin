c31c33435fcc4b7492f3608d10d3a7c388d10001
Conqcd <411277487@qq.com>
2021-12-13 23:07:03 +0800

    info

--- rengine-src/Rengine/Platform/OpenGL/OpenGLContext.cpp
+++ rengine-src/Rengine/Platform/OpenGL/OpenGLContext.cpp
@@ -4,7 +4,7 @@
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-
+// #include <gl/GL.h>
 namespace Rengin
 {
 OpenGLContext::OpenGLContext(GLFWwindow* win):m_win(win)
@@ -21,6 +21,11 @@ void OpenGLContext::Init()
     glfwMakeContextCurrent(m_win);
     int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
     RE_CORE_ASSERT(status,"failed to intialize Glad");
+
+    RE_CORE_INFO("OpenGL Info :");
+    RE_CORE_INFO("    Vendor : {0}",glGetString(GL_VENDOR));
+    RE_CORE_INFO("    Renderer : {0}",glGetString(GL_RENDERER));
+    RE_CORE_INFO("    Version : {0}",glGetString(GL_VERSION));
 }
 
 void OpenGLContext::SwapBuffer()
