c1b698382df4cf96d1ecab6958709761aef520b7
Conqcd <411277487@qq.com>
2021-12-22 11:02:56 +0800

    texture for 2drenderer

--- rengine-src/Rengine/Platform/OpenGL/OpenGLTexture.cpp
+++ rengine-src/Rengine/Platform/OpenGL/OpenGLTexture.cpp
@@ -37,6 +37,10 @@ OpenGLTexture2D::OpenGLTexture2D(const std::string& path)
     glTextureParameteri(m_render_id,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
     glTextureParameteri(m_render_id,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
 
+    glTextureParameteri(m_render_id,GL_TEXTURE_WRAP_S,GL_REPEAT);
+    glTextureParameteri(m_render_id,GL_TEXTURE_WRAP_T,GL_REPEAT);
+
+
     glTextureSubImage2D(m_render_id,0,0,0,m_width,m_height,dataFormat,GL_UNSIGNED_BYTE,data);
 
     stbi_image_free(data);
