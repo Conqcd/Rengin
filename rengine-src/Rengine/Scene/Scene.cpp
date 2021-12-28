3d43805e2b57c6a11eb3f6f9f56a3555f6b7c0eb
Conqcd <411277487@qq.com>
2021-12-28 20:51:48 +0800

    entt

---
+++ rengine-src/Rengine/Scene/Scene.cpp
@@ -0,0 +1,41 @@
+#include "repch.hpp"
+#include "Scene.hpp"
+
+#include <glm/glm.hpp>
+
+namespace Rengin
+{
+
+static void DoMath(const glm::mat4& transform)
+{
+
+}
+
+Scene::Scene(/* args */)
+{
+    struct TransformComponent
+    {
+        glm::mat4 Transform;
+        TransformComponent() = default;
+        TransformComponent(const TransformComponent&) = default;
+        TransformComponent(const glm::mat4& transform)
+            :Transform(transform)   {}
+
+        operator const glm::mat4& () const {return Transform;}
+        operator glm::mat4& () {return Transform;}
+    };
+    TransformComponent transform;
+
+    DoMath(transform);
+
+    entt::entity entity_ = m_registry.create();
+
+    m_registry.emplace<>()
+
+}
+
+Scene::~Scene()
+{
+}
+
+} // namespace Rengin
