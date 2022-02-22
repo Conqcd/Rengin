394bab0c7b455f039730f9c0e36b65d7029469fb
Conqcd <411277487@qq.com>
2022-02-22 11:21:21 +0800

    browser panel

---
+++ Rengine-Editor/Panels/ContentBrowserPanel.cpp
@@ -0,0 +1,10 @@
+#include <repch.hpp>
+#include "ContentBrowserPanel.hpp"
+
+namespace Rengin {
+void ContentBrowserPanel::ImGuiRender()
+{
+
+}
+
+} // namespace Rengin
---
+++ Rengine-Editor/Panels/ContentBrowserPanel.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace Rengin
+{
+class ContentBrowserPanel
+{
+private:
+    /* data */
+public:
+    ContentBrowserPanel(/* args */) = default;
+    ~ContentBrowserPanel() = default;
+
+    void ImGuiRender();
+};
+
+
+} // namespace Rengin
