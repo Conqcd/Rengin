2e23f5fc1e9afc8ff8b3b0da5f8a8332989f1296
Conqcd <411277487@qq.com>
2022-03-18 20:46:35 +0800

    wait for light

--- rengine-src/Rengine/Renderer/Methods/PhongMethod.hpp
+++ rengine-src/Rengine/Renderer/Methods/PhongMethod.hpp
@@ -1,14 +1,16 @@
 #pragma once
 #include "Rengine/Core/core.hpp"
 #include "BaseMethod.hpp"
+#include "Rengine/Scene/ObjManager.hpp"
 
 namespace Rengin
 {
 class PhongMethod : public RenderMethod
 {
 private:
+
 public:
-    PhongMethod(/* args */);
+    PhongMethod();
     virtual ~PhongMethod();
 
     virtual void Render() override; 
