e0f952db9e9ebfabdca9e606f0bcd90894ce7a4f
Conqcd <411277487@qq.com>
2022-03-19 17:17:29 +0800

    new phong

---
+++ Rengine-Editor/assets/shaders/ShadowPhongF.glsl
@@ -0,0 +1,59 @@
+#version 330
+
+in vec3 v_position;
+in vec3 v_normal;
+in vec2 v_texCoords;
+in vec4 v_PositionFromLight;
+
+layout(location = 0) out vec4 o_color;
+layout(location = 1) out int o_Entity;
+
+uniform int u_Entity;
+uniform sampler2D m_texture;
+uniform sampler2D u_ShadowMap;
+
+//Camera
+uniform vec3 u_CameraPos;
+
+// Light
+uniform vec3 u_LightPos;
+uniform vec3 u_LightIntensity;
+
+//Material
+uniform vec3 u_Ks;
+uniform vec3 u_Kd;
+uniform vec3 u_Ka;
+uniform float u_Ns;
+uniform float u_Ni;
+
+vec3 BlinnPhong()
+{
+    vec3 color = texture2D(m_texture,v_texCoords).rgb;
+    color = pow(color, vec3(2.2));
+
+    vec3 ambient = 0.05 * color;
+
+    vec3 lightDir = normalize(u_LightPos);
+    vec3 normal = normalize(v_normal);
+    float diff = max(dot(lightDir, normal), 0.0);
+    vec3 light_atten_coff = u_LightIntensity / pow(length(u_LightPos - v_position), 2.0);
+    vec3 diffuse = diff * light_atten_coff * color;
+
+    vec3 viewDir = normalize(u_CameraPos - v_position);
+    vec3 halfDir = normalize((lightDir + viewDir));
+    float spec = pow(max(dot(halfDir, normal), 0.0), u_Ns);
+    vec3 specular = u_Ks * light_atten_coff * spec;
+
+    vec3 radiance = (ambient + diffuse + specular);
+    
+    vec3 phongColor = pow(radiance, vec3(1.0 / 2.2));
+    return phongColor;
+}
+
+void main()
+{    
+    float visibility;
+    // visibility = useShadowMap(u_ShadowMap, v_PositionFromLight);
+    o_color = vec4(BlinnPhong(),1.0);
+    o_Entity = u_Entity;
+}
---
+++ Rengine-Editor/assets/shaders/ShadowPhongV.glsl
@@ -0,0 +1,24 @@
+#version 330
+
+layout(location = 0) in vec3 a_position;
+layout(location = 1) in vec3 a_normal;
+layout(location = 2) in vec2 a_texCoords;
+
+uniform mat4 u_View;
+uniform mat4 u_Projection;
+uniform mat4 u_Transform;
+uniform mat4 uLightMVP;
+
+out vec3 v_position;
+out vec3 v_normal;
+out vec2 v_texCoords;
+out vec4 v_PositionFromLight;
+
+void main()
+{  
+    v_position = (u_Transform * vec4(a_position, 1.0)).xyz;
+    v_normal = (u_Transform * vec4(a_normal, 0.0)).xyz;
+    v_texCoords = a_texCoords;
+    v_PositionFromLight = uLightMVP * vec4(a_position, 1.0);
+    gl_Position = u_Projection * u_View * u_Transform * vec4(a_position,1.0);
+}
